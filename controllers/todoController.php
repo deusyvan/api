e9220b35f622353369cf42c76bbb814403841551
root <root@dfsweb.com.br>
2020-01-04 12:12:44 -0300

    Criando o backend para nosso webservice

--- controllers/todoController.php
+++ controllers/todoController.php
@@ -7,13 +7,41 @@ class todoController extends controller{
     
     public function index(){}
     
-    public function listar(){}
+    public function listar(){
+        $array = array();
+        $t = new Tarefas();
+        $array = $t->listar();
+        
+        header("Content-Type: application/json");
+        echo json_encode($array);
+    }
     
-    public function add(){}
+    public function add(){
+        if(isset($_POST['titulo']) && !empty($_POST['titulo'])){
+            $titulo = addslashes($_POST['titulo']);
+            $t = new Tarefas();
+            $t->addTarefa($titulo);
+        }
+    }
     
-    public function del(){}
+    public function del(){
+        if(isset($_POST['id']) && !empty($_POST['id'])){
+            $id = addslashes($_POST['id']);
+            
+            $t = new Tarefas();
+            $t->delTarefa($id);
+        }
+    }
     
-    public function update(){}
+    public function update(){
+        if(isset($_POST['id']) && !empty($_POST['id'])){
+            $id = addslashes($_POST['id']);
+            $status = addslashes($_POST['status']);
+            
+            $t = new Tarefas();
+            $t->updateStatus($status, $id);
+        }
+    }
     
 }
 
--- models/Tarefas.php
+++ models/Tarefas.php
@@ -15,7 +15,18 @@ class Tarefas extends model{
     
     public function addTarefa($titulo) {
         $this->db->query("INSERT INTO tarefas SET titulo = '$titulo'");
+        /* $sql = $this->db->prepare("INSERT INTO tarefas SET titulo = :titulo");
+        $sql->bindValue(":titulo", $titulo);
+        $sql->execute();
+        return $sql->rowCount(); */
+        
     }
     
+    public function delTarefa($id) {
+        $this->db->query("DELETE FROM tarefas WHERE id = '$id'");
+    }
     
+    public function updateStatus($status, $id) {
+        $this->db->query("UPDATE tarefas SET status = '$status' WHERE id = '$id'");
+    }
 }
